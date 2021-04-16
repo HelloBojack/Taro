8a40a13520cd4884b98cacca7ad4233327947a86
bojack <695668678@qq.com>
2021-04-16 12:06:16 +0800

    音乐播放器

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Taro
+
+![image](https://s31.aconvert.com/convert/p3r68-cdx67/3ihnn-livtt.gif)
--- demo-01/src/pages/profile/index.tsx
+++ demo-01/src/pages/profile/index.tsx
@@ -9,20 +9,52 @@ import { loginByPhone } from '@api/api'
 import './index.scss'
 
 export default function Index() {
-  const [usernameValue, setUsernameValue] = useState('')
-  const handleChange = (value) => {
-    setUsernameValue(value)
+  const [loginForm, setLoginForm] = useState({
+    usernameValue: '',
+    passwordValue: ''
+  })
+
+  const handleUsernameValueChange = (e) => {
+    console.log(e)
+
   }
-  const onSubmit = () => {
-    console.log(usernameValue)
+  const handleSumbit = async () => {
+    const result = await loginByPhone(loginForm);
+    console.log(loginForm)
+    // console.log(passwordValue)
   }
-  // const onReset: (event) => {
-  //   setUsernameValue('')
-  // }
+
+
   return (
     <View className='profile'>
-
+      <View>
+        <AtInput
+          name='usernameValue'
+          type='text'
+          placeholder='请输入用户名'
+          value={loginForm.usernameValue}
+          onChange={e => {
+            setLoginForm({
+              ...loginForm,
+              usernameValue: e
+            })
+          }}
+        />
+        <AtInput
+          name='passwordValue'
+          type='password'
+          placeholder='密码不能少于10位数'
+          value={loginForm.passwordValue}
+          onChange={e => {
+            setLoginForm({
+              ...loginForm,
+              passwordValue: e
+            })
+          }}
+        />
+        <AtButton type='primary' onClick={handleSumbit}>登录</AtButton>
+      </View>
       <TabBarPro></TabBarPro>
-    </View>
+    </View >
   )
 }
